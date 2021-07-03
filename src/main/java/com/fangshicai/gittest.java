dcdccd0de026b7d0ba0ea4fd1f01192030058eee
fangshicai <fangshicai@fangshicai.com>
2021-07-03 21:04:42 +0800

    master

--- src/main/java/com/fangshicai/gittest.java
+++ src/main/java/com/fangshicai/gittest.java
@@ -10,5 +10,7 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
