3f198a32fcf555119f166a6135d64741c3a311c0
fangshicai <fangshicai@fangshicai.com>
2021-07-03 20:35:33 +0800

    first

---
+++ src/main/java/com/fangshicai/gittest.java
@@ -0,0 +1,13 @@
+package com.fangshicai;
+
+/**
+ * @ClassName gittest
+ * @Description TODO
+ * @Author fangshicai
+ * @Date 2021/7/3 20:27
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
